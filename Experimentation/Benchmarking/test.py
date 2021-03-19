34e0df34677bbcc38e267e2bfa3d8943d8a7a8cb
ClaartjeBarkhof <claartjebarkhof@hotmail.com>
2021-03-19 14:09:25 +0100

    dont track experimentation with optimus code

--- Experimentation/Benchmarking/test.py
+++
@@ -1,8 +0,0 @@
-from transformers import BertConfig, BertModel
-import torch
-
-print("torch.cuda.is_available()",torch.cuda.is_available())
-print("torch.cuda.current_device()", torch.cuda.current_device())
-
-# Download model and configuration from S3 and cache.
-model = BertModel.from_pretrained('bert-base-uncased')
--- Experimentation/Optimus/code/scripts/scripts_local/run_generation_gpt2.sh
+++
@@ -1,4 +0,0 @@
-python ./examples/run_generation.py \
-    --model_type=gpt2 \
-    --length=20 \
-    --model_name_or_path=gpt2 \
